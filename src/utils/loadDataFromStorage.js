8290ea7c1d739ef120f62f5c7b728374e563adeb
marcoskloss <marcoskloss2001@gmail.com>
2021-03-31 08:26:08 -0300

    fix localstorage bug

--- src/utils/loadDataFromStorage.js
+++ src/utils/loadDataFromStorage.js
@@ -1,5 +1,6 @@
 export function loadDataFromStorage(key) {
   const currentState = localStorage.getItem(key)
+  console.log(currentState);
   if (currentState) {
     return JSON.parse(currentState)
   } else {
--- src/utils/persistState.js
+++ src/utils/persistState.js
@@ -1,3 +1,3 @@
 export function persistState(key, value) {
-  localStorage.setItem(key, value)
+  localStorage.setItem(key, JSON.stringify(value))
 }
